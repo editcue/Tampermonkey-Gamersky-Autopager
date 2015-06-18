b7a5a6f6f44f1bff16ce0f7ef43d807583900933
editcue@gmail.com <editcue@gmail.com>
2015-06-18 19:49:08 +0800

    init

--- src/js/TampermonkeyScript.js
+++ src/js/TampermonkeyScript.js
@@ -1,6 +1,16 @@
 /**
  * Created by editcue on 2015/6/18.
  */
+// ==UserScript==
+// @name         Gamersky Autopager
+// @namespace    https://github.com/editcue/TampermonkeyGamerskyAutopager/
+// @version      0.1
+// @description  enter something useful
+// @author       editcue@gmail.com
+// @match        http://www.gamersky.com/ent/*/*.shtml
+// @grant        none
+// @require 	 http://code.jquery.com/jquery-2.1.1.min.js
+// ==/UserScript==
 $(function () {
     $.get('https://raw.githubusercontent.com/editcue/TampermonkeyGamerskyAutopager/master/src/autopager.html',
         function (domStr, msg) {
