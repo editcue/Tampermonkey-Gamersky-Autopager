f8399527fe4f92c0c64881afc5957e5c9dc318ce
ray <editcue@hotmail.com>
2018-06-05 12:18:32 +0800

    fix handbook page video loading script

--- gamersky_autopager.js
+++ gamersky_autopager.js
@@ -4,7 +4,7 @@
 // ==UserScript==
 // @name         Gamersky Autopager
 // @namespace    https://github.com/editcue/Tampermonkey-Gamersky-Autopager
-// @version      0.7
+// @version      0.8
 // @description  游民星空加载下一页插件
 // @author       editcue@gmail.com
 // @match        http://www.gamersky.com/ent/*/*.shtml
