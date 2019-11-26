1b2ed21c429c387481551c276c4cf495ef7d6f03
ray <editcue@hotmail.com>
2019-11-26 12:58:44 +0800

    add url support //shouyou.gamersky.com/news/*/*.shtml

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [Tampermonkey Gamersky Autopager](https://github.com/editcue/Tampermonkey-Gamersky-Autopager)
 
-* version：0.83
+* version：0.84
 * Email: [editcue@gmail.com](editcue@gmail.com)
 
 这是一个（油猴）脚本库。使用前,浏览器需要先安装油猴插件。
--- gamersky_autopager.js
+++ gamersky_autopager.js
@@ -4,7 +4,7 @@
 // ==UserScript==
 // @name         Gamersky Autopager
 // @namespace    https://github.com/editcue/Tampermonkey-Gamersky-Autopager
-// @version      0.83
+// @version      0.84
 // @description  游民星空加载下一页插件
 // @author       editcue@gmail.com
 // @match       *://www.gamersky.com/ent/*/*.shtml
@@ -20,6 +20,7 @@
 // @match       *://acg.gamersky.com/otaku/*/*.shtml
 // @match       *://acg.gamersky.com/pic/*/*.shtml
 // @match       *://acg.gamersky.com/music/*/*.shtml
+// @match       *://shouyou.gamersky.com/news/*/*.shtml
 // @grant        none
 // @require 	 https://cdn.bootcss.com/jquery/3.2.1/jquery.js
 // ==/UserScript==
